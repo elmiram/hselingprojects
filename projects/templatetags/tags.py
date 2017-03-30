b73d731823eccdde22b677d0c0a31398458e674c
Elmira <cclikespizza@gmail.com>
2017-03-30 05:04:43 +0300

    fix lang_tags for server

--- projects/templatetags/tags.py
+++ projects/templatetags/tags.py
@@ -5,10 +5,10 @@ register = template.Library()
 
 @register.simple_tag
 def lang_link(value, lang):
-    value = value[3:] if value.startswith('/en/') else value
-    lang = '/en' if lang == 'en' else ''
     prefix = ''
     if value.startswith('/projects'):
         prefix = '/projects'
         value = value[9:]
+    value = value[3:] if value.startswith('/en/') else value
+    lang = '/en' if lang == 'en' else ''
     return prefix + lang + value
